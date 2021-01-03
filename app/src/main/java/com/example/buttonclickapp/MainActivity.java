b8bdb02e1d6c7d30141ece0c83a9519bda8884e8
Grzegorz Konopka <41437422+konopkagrzegorz@users.noreply.github.com>
2021-01-03 18:47:36 +0100

    Fixed onClick() method listener issue with button

--- app/src/main/java/com/example/buttonclickapp/MainActivity.java
+++ app/src/main/java/com/example/buttonclickapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.buttonclickapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -12,12 +14,32 @@ public class MainActivity extends AppCompatActivity {
     private Button button;
     private EditText editText;
     private TextView textView;
+    private int counter = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = findViewById(R.id.button);
-        editText = findViewById(R.id.editText);
-        textView = findViewById(R.id.textView);
+        button = (Button) findViewById(R.id.button);
+        editText = (EditText) findViewById(R.id.editText);
+        textView = (TextView) findViewById(R.id.textView);
+        editText.setText("");
+        textView.setText("");
+        textView.setMovementMethod(new ScrollingMovementMethod());
+        View.OnClickListener counterOnClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!(editText.getText().toString().equals(""))) {
+                    counter++;
+                    String result = editText.getText().toString() + " was pressed " + counter + " time";
+                    if (counter > 1) {
+                        result += "s";
+                    }
+                    result += ".\n";
+                    textView.append(result);
+                    editText.setText("");
+                }
+            }
+        };
+        button.setOnClickListener(counterOnClickListener);
     }
 }
