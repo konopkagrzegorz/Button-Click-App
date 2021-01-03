d18d04995531096998124c9d688bd34db62c22b1
Grzegorz Konopka <41437422+konopkagrzegorz@users.noreply.github.com>
2021-01-03 16:01:19 +0100

    Initial commit

---
+++ app/src/main/java/com/example/buttonclickapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.buttonclickapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private EditText editText;
+    private TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        editText = findViewById(R.id.editText);
+        textView = findViewById(R.id.textView);
+    }
+}
